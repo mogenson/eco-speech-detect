5ddbbbee5fde64542a23f77b832c7b93014740a6
Mike Mogenson <michael.mogenson@leaflabs.com>
2017-02-14 16:00:44 -0500

    ctrl-c to kill webserver

--- run_me.sh
+++ run_me.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+ctrl_c()
+{
+    echo "killing web server"
+    kill $WEB_SERVER_PID
+    exit 0
+}
+
+trap ctrl_c SIGINT
+
 python -m SimpleHTTPServer &
+
+WEB_SERVER_PID=$!
 
 sleep 1
 
 google-chrome http://0.0.0.0:8000
+
+while true
+do
+    sleep 1
+done
